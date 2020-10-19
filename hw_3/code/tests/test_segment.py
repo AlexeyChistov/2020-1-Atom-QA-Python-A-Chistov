543bf73332867ebd4b753e70b0a1244269daa8ac
AlexeyChistov <alvalchist@gmail.com>
2020-10-19 22:39:52 +0300

    Забыл про маркеры API, добавил

--- hw_3/code/tests/test_segment.py
+++ hw_3/code/tests/test_segment.py
@@ -1,14 +1,15 @@
 from conftest import BaseCase
-
+import pytest
 
 class TestClass(BaseCase):
-
+    @pytest.mark.API
     def test_create_segment(self, segment_title):
         """Тестируем создание сегмента"""
         segment_id = self.my_target_client.create_segment(segment_title)
         response = self.my_target_client.checking_segment_creation(segment_id)
         assert response == 200
 
+    @pytest.mark.API
     def test_create_and_delete_segment(self, segment_title):
         """Тестируем создание и удаление сегиента"""
         segment_id = self.my_target_client.create_segment(segment_title)
